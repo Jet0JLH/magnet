67799ecac9f717617263bc1b8575b2aba998d1e7
Jet0JLH <Jet0JLH@gmx.de>
2018-10-09 10:28:05 +0200

    implement writeLine

--- magnet-server/magnet-server.vb
+++ magnet-server/magnet-server.vb
@@ -1,5 +1,21 @@
 ﻿Module magnet_server
-
+    Sub wl(ByVal text As String, Optional ByVal mode As wlModes = 0, Optional ByVal forceLog As Boolean = False) 'WriteLine
+        Select Case mode
+            Case wlModes.Info
+                Console.ForegroundColor = ConsoleColor.White
+            Case wlModes.Warning
+                Console.ForegroundColor = ConsoleColor.Yellow
+            Case wlModes.Critical
+                Console.ForegroundColor = ConsoleColor.Red
+        End Select
+        Console.WriteLine(text)
+        Console.ForegroundColor = ConsoleColor.White
+    End Sub
+    Enum wlModes 'WriteLine Modes
+        Info = 0
+        Warning = 1
+        Critical = 2
+    End Enum
     Sub Main()
 
     End Sub
