1467d84a1e7ff273274133f5409ad45205b45ce5
Jet0JLH <Jet0JLH@gmx.de>
2018-10-09 11:16:28 +0200

    load, check and generate server rudimentary config

--- magnet-client/magnet-client.vb
+++ magnet-client/magnet-client.vb
@@ -1,5 +1,5 @@
 ﻿Module magnet_client
-    Sub wl(ByVal text As String, Optional ByVal mode As wlModes = 0, Optional ByVal forceLog As Boolean = False) 'WriteLine
+    Sub wl(ByVal text As String, Optional ByVal mode As wlModes = 0, Optional ByVal isFatal As Boolean = False, Optional ByVal forceLog As Boolean = False) 'WriteLine
         Select Case mode
             Case wlModes.Info
                 Console.ForegroundColor = ConsoleColor.White
@@ -9,6 +9,7 @@
                 Console.ForegroundColor = ConsoleColor.Red
         End Select
         Console.WriteLine(text)
+        If isFatal Then Stop
         Console.ForegroundColor = ConsoleColor.White
     End Sub
     Enum wlModes 'WriteLine Modes
--- magnet-server/magnet-server.vb
+++ magnet-server/magnet-server.vb
@@ -1,5 +1,5 @@
 ﻿Module magnet_server
-    Sub wl(ByVal text As String, Optional ByVal mode As wlModes = 0, Optional ByVal forceLog As Boolean = False) 'WriteLine
+    Sub wl(ByVal text As String, Optional ByVal mode As wlModes = 0, Optional ByVal isFatal As Boolean = False, Optional ByVal forceLog As Boolean = False) 'WriteLine
         Select Case mode
             Case wlModes.Info
                 Console.ForegroundColor = ConsoleColor.White
@@ -9,6 +9,7 @@
                 Console.ForegroundColor = ConsoleColor.Red
         End Select
         Console.WriteLine(text)
+        If isFatal Then Stop
         Console.ForegroundColor = ConsoleColor.White
     End Sub
     Enum wlModes 'WriteLine Modes
@@ -16,8 +17,48 @@
         Warning = 1
         Critical = 2
     End Enum
+    Const configPath As String = "magnet-server.conf"
+    Dim logPath As String = ""
+    Dim port As String = ""
     Sub Main()
-
+        wl("Set Workingdirectory...")
+        My.Computer.FileSystem.CurrentDirectory = IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
+        wl("Check config...")
+        If checkConfig() <> 0 Then wl("Generate config...") : If generateConfig() <> 0 Then wl("Unable to generate config", wlModes.Critical, True)
+        wl("Startup magnet-server v" & My.Application.Info.Version.ToString, wlModes.Info, False, True)
     End Sub
+    Function checkConfig() As Byte
+        If My.Computer.FileSystem.FileExists(configPath) Then
+            Try
+                Dim tempXML As XDocument = XDocument.Load(configPath)
+                Try
+                    Dim temp As String = ""
+                    With tempXML.Element("conf")
+                        logPath = .Element("logPath").Value
+                        With .Element("service")
+                            port = .Element("port")
+                        End With
+                    End With
+
+                    Return 0
+                Catch ex As Exception
+                    Return 3
+                End Try
+            Catch ex As Exception
+                Return 2
+            End Try
+        Else
+            Return 1
+        End If
+    End Function
+    Function generateConfig() As Byte
+        Dim tempXML As New XDocument(<conf><logPath>magnet-server.log</logPath><service><port>8894</port></service></conf>)
+        Try
+            tempXML.Save(configPath)
+            Return 0
+        Catch ex As Exception
+            Return 1
+        End Try
+    End Function
 
 End Module
